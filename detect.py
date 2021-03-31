06b7926b35483897401748aa015bb9f9664cc820
stanleydukor <stanleydukor@gmail.com>
2021-03-31 04:19:30 +0100

    arrange files

--- detect.py
+++ detect.py
@@ -5,19 +5,18 @@ from PIL import Image, ImageDraw, ImageFont
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load model checkpoint
-checkpoint = 'checkpoint/checkpoint_ssd300.pth.tar'
+checkpoint = os.path.join("checkpoint", os.listdir("checkpoint")[-1])
 checkpoint = torch.load(checkpoint)
-start_epoch = checkpoint['epoch'] + 1
-print('\nLoaded checkpoint from epoch %d.\n' % start_epoch)
-model = checkpoint['model']
+start_epoch = checkpoint["epoch"] + 1
+print("\nLoaded checkpoint from epoch %d.\n" % start_epoch)
+model = checkpoint["model"]
 model = model.to(device)
 model.eval()
 
 # Transforms
 resize = transforms.Resize((300, 300))
 to_tensor = transforms.ToTensor()
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
 
 def detect(original_image, min_score, max_overlap, top_k, suppress=None):
@@ -42,22 +41,33 @@ def detect(original_image, min_score, max_overlap, top_k, suppress=None):
     predicted_locs, predicted_scores = model(image.unsqueeze(0))
 
     # Detect objects in SSD output
-    det_boxes, det_labels, det_scores = model.detect_objects(predicted_locs, predicted_scores, min_score=min_score,
-                                                             max_overlap=max_overlap, top_k=top_k)
+    det_boxes, det_labels, det_scores = model.detect_objects(
+        predicted_locs,
+        predicted_scores,
+        min_score=min_score,
+        max_overlap=max_overlap,
+        top_k=top_k,
+    )
 
     # Move detections to the CPU
-    det_boxes = det_boxes[0].to('cpu')
+    det_boxes = det_boxes[0].to("cpu")
 
     # Transform to original image dimensions
     original_dims = torch.FloatTensor(
-        [original_image.width, original_image.height, original_image.width, original_image.height]).unsqueeze(0)
+        [
+            original_image.width,
+            original_image.height,
+            original_image.width,
+            original_image.height,
+        ]
+    ).unsqueeze(0)
     det_boxes = det_boxes * original_dims
 
     # Decode class integer labels
-    det_labels = [rev_label_map[l] for l in det_labels[0].to('cpu').tolist()]
+    det_labels = [rev_label_map[l] for l in det_labels[0].to("cpu").tolist()]
 
     # If no objects found, the detected labels will be set to ['0.'], i.e. ['background'] in SSD300.detect_objects() in model.py
-    if det_labels == ['background']:
+    if det_labels == ["background"]:
         # Just return original image
         return original_image
 
@@ -76,30 +86,36 @@ def detect(original_image, min_score, max_overlap, top_k, suppress=None):
         # Boxes
         box_location = det_boxes[i].tolist()
         draw.rectangle(xy=box_location, outline=label_color_map[det_labels[i]])
-        draw.rectangle(xy=[l + 1. for l in box_location], outline=label_color_map[
-            det_labels[i]])  # a second rectangle at an offset of 1 pixel to increase line thickness
-        draw.rectangle(xy=[l + 2. for l in box_location], outline=label_color_map[
-            det_labels[i]])  # a third rectangle at an offset of 1 pixel to increase line thickness
-        draw.rectangle(xy=[l + 3. for l in box_location], outline=label_color_map[
-            det_labels[i]])  # a fourth rectangle at an offset of 1 pixel to increase line thickness
+        draw.rectangle(
+            xy=[l + 1.0 for l in box_location], outline=label_color_map[det_labels[i]]
+        )  # a second rectangle at an offset of 1 pixel to increase line thickness
+        draw.rectangle(
+            xy=[l + 2.0 for l in box_location], outline=label_color_map[det_labels[i]]
+        )  # a third rectangle at an offset of 1 pixel to increase line thickness
+        draw.rectangle(
+            xy=[l + 3.0 for l in box_location], outline=label_color_map[det_labels[i]]
+        )  # a fourth rectangle at an offset of 1 pixel to increase line thickness
 
         # Text
         text_size = font.getsize(det_labels[i].upper())
-        text_location = [box_location[0] + 2., box_location[1] - text_size[1]]
-        textbox_location = [box_location[0], box_location[1] - text_size[1], box_location[0] + text_size[0] + 4.,
-                            box_location[1]]
+        text_location = [box_location[0] + 2.0, box_location[1] - text_size[1]]
+        textbox_location = [
+            box_location[0],
+            box_location[1] - text_size[1],
+            box_location[0] + text_size[0] + 4.0,
+            box_location[1],
+        ]
         draw.rectangle(xy=textbox_location, fill=label_color_map[det_labels[i]])
-        draw.text(xy=text_location, text=det_labels[i].upper(), fill='white',
-                  font=font)
+        draw.text(xy=text_location, text=det_labels[i].upper(), fill="white", font=font)
     del draw
 
-    # annotated_image.save('output/result.jpg')
+    annotated_image.save("output/result.jpg")
 
     return annotated_image
 
 
-if __name__ == '__main__':
-    img_path = 'img/1.jpg'
-    original_image = Image.open(img_path, mode='r')
-    original_image = original_image.convert('RGB')
+if __name__ == "__main__":
+    img_path = "img/3.jpg"
+    original_image = Image.open(img_path, mode="r")
+    original_image = original_image.convert("RGB")
     detect(original_image, min_score=0.2, max_overlap=0.5, top_k=200).show()
--- test_video.py
+++ test_video.py
@@ -8,17 +8,16 @@ from PIL import Image, ImageDraw, ImageFont
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load model checkpoint
-checkpoint = 'checkpoint/checkpoint_ssd300.pth.tar'
+checkpoint = os.path.join("checkpoint", os.listdir("checkpoint")[-1])
 checkpoint = torch.load(checkpoint)
-model = checkpoint['model']
+model = checkpoint["model"]
 model = model.to(device)
 model.eval()
 
 # Transforms
 resize = transforms.Resize((300, 300))
 to_tensor = transforms.ToTensor()
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
 
 def detect(original_image, min_score, max_overlap, top_k, storage, suppress=None):
@@ -32,22 +31,33 @@ def detect(original_image, min_score, max_overlap, top_k, storage, suppress=None
     predicted_locs, predicted_scores = model(image.unsqueeze(0))
 
     # Detect objects in SSD output
-    det_boxes, det_labels, det_scores = model.detect_objects(predicted_locs, predicted_scores, min_score=min_score,
-                                                             max_overlap=max_overlap, top_k=top_k)
+    det_boxes, det_labels, det_scores = model.detect_objects(
+        predicted_locs,
+        predicted_scores,
+        min_score=min_score,
+        max_overlap=max_overlap,
+        top_k=top_k,
+    )
 
     # Move detections to the CPU
-    det_boxes = det_boxes[0].to('cpu')
+    det_boxes = det_boxes[0].to("cpu")
 
     # Transform to original image dimensions
     original_dims = torch.FloatTensor(
-        [original_image.width, original_image.height, original_image.width, original_image.height]).unsqueeze(0)
+        [
+            original_image.width,
+            original_image.height,
+            original_image.width,
+            original_image.height,
+        ]
+    ).unsqueeze(0)
     det_boxes = det_boxes * original_dims
 
     # Decode class integer labels
-    det_labels = [rev_label_map[l] for l in det_labels[0].to('cpu').tolist()]
+    det_labels = [rev_label_map[l] for l in det_labels[0].to("cpu").tolist()]
 
     # If no objects found, the detected labels will be set to ['0.'], i.e. ['background'] in SSD300.detect_objects() in model.py
-    if det_labels == ['background']:
+    if det_labels == ["background"]:
         # Just return original image
         return original_image
 
@@ -70,26 +80,31 @@ def detect(original_image, min_score, max_overlap, top_k, storage, suppress=None
             print(i)
             print(det_boxes[i].shape, storage[i].shape)
             diff = abs(det_boxes[i] - storage[i])
-            if sum((diff>20).type(torch.uint8)) == 0:
+            if sum((diff > 20).type(torch.uint8)) == 0:
                 det_boxes[i] = storage[i]
 
         # Boxes
         box_location = det_boxes[i].tolist()
         draw.rectangle(xy=box_location, outline=label_color_map[det_labels[i]])
-        draw.rectangle(xy=[l + 1. for l in box_location], outline=label_color_map[
-            det_labels[i]])  # a second rectangle at an offset of 1 pixel to increase line thickness
+        draw.rectangle(
+            xy=[l + 1.0 for l in box_location], outline=label_color_map[det_labels[i]]
+        )  # a second rectangle at an offset of 1 pixel to increase line thickness
 
         # Text
         text_size = font.getsize(det_labels[i].upper())
-        text_location = [box_location[0] + 2., box_location[1] - text_size[1]]
-        textbox_location = [box_location[0], box_location[1] - text_size[1], box_location[0] + text_size[0] + 4.,
-                            box_location[1]]
+        text_location = [box_location[0] + 2.0, box_location[1] - text_size[1]]
+        textbox_location = [
+            box_location[0],
+            box_location[1] - text_size[1],
+            box_location[0] + text_size[0] + 4.0,
+            box_location[1],
+        ]
         draw.rectangle(xy=textbox_location, fill=label_color_map[det_labels[i]])
-        draw.text(xy=text_location, text=det_labels[i].upper(), fill='white',
-                  font=font)
+        draw.text(xy=text_location, text=det_labels[i].upper(), fill="white", font=font)
     del draw
 
     return annotated_image, det_boxes
+
 
 currentFrame = 0
 cap = cv2.VideoCapture("http://192.168.43.1:8080/video")
@@ -100,13 +115,15 @@ while True:
     if not ret:
         break
     pil_img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    result, storage = detect(pil_img, min_score=0.5, max_overlap=0.5, top_k=200, storage=storage)
-    result = np.array(result) 
-    cv2.imshow('frame',result)
+    result, storage = detect(
+        pil_img, min_score=0.5, max_overlap=0.5, top_k=200, storage=storage
+    )
+    result = np.array(result)
+    cv2.imshow("frame", result)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
-        
+
     currentFrame += 1
 
 cap.release()
